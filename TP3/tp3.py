9aeaaf3cfdb0f961d73516145b430db26d1f361a
sebacarmar <sebacarmar@gmail.com>
2024-04-17 10:46:13 -0300

    Agrega version final del tp3

--- TP3/tp3.py
+++ TP3/tp3.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python3
 #-*-coding:utf-8-*-
 
-
 import cv2
+import sys
 
-cap = cv2.VideoCapture(0)
+if(len(sys.argv) > 1):
+    filename = sys.argv[1]
+else:
+    print('Ingrese un archivo de vídeo como argumento')
+    sys.exit(0)
 
+cap       = cv2.VideoCapture(filename)
 
-fps   = cap.get(cv2.CAP_PROP_FPS)
-alto  = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-ancho = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+fourcc    = cv2.VideoWriter_fourcc('X','V','I','D') #Formato
+fps       = cap.get(cv2.CAP_PROP_FPS)
+alto      = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+ancho     = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+out       = cv2.VideoWriter('videoOut.mkv', fourcc, fps, (ancho,alto))
 
-#ret, frame = cap.read()
-#alto,ancho = frame.shape[:2]
+delay     = int((1/fps)*1000)
+
+while( cap.isOpened() ):
+    (ret,img) = cap.read()
+
+    if( ret is True ):
+        #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        out.write(img)
+        cv2.imshow('Mi video', img)
+        if( (cv2.waitKey(delay) & 0xFF) == ord('q') ):
+            break
+    else:
+        break
 
 cap.release()
-
-print('El valor de fps de la cámara es:', fps)
-print('El valor de ancho y alto es de la cámara es:', ancho, 'x', alto)
-
+out.release()
 cv2.destroyAllWindows()
-
-
