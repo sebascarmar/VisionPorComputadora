0378442e0136ec768edfe919371b1783aae475f0
sebacarmar <sebacarmar@gmail.com>
2024-04-24 12:17:08 -0300

    Agrega primera versión del tp4

---
+++ TP4/tp4.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+drawing  = False 
+mode     = True 
+(xi, yi) = (-1, -1)
+
+######################## FUNCIONES #######################
+
+def select_image(event, x, y, flags, param):
+    global xi, yi, drawing, mode
+    if(event == cv2.EVENT_LBUTTONDOWN):
+        drawing  = True
+        (xi, yi) = (x, y)
+    elif(event == cv2.EVENT_MOUSEMOVE):
+        if(drawing is True):
+            img_aux = img.copy()
+            cv2.rectangle(img_aux, (xi,yi), (x,y), (0,255,0), 1)
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+
+
+########################## MAIN ##########################
+
+img = cv2.imread ('../TP2/hoja.png', 1)
+#img = np.zeros((512,512,3), np.uint8)
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', select_image)
+
+while(1):
+    cv2.imshow('image', img )
+    k = cv2.waitKey(1) & 0xFF
+    if(k == ord('m')):
+        mode = not mode
+    elif(k == 27):
+        break
+
+cv2.destroyAllWindows()
+
+
+
+
