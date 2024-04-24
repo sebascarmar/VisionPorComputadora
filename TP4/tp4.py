7ae6293d19a0bb56d5c2f65a2d0df2a1a97e12e9
sebacarmar <sebacarmar@gmail.com>
2024-04-24 13:20:25 -0300

    Modifica cómo se muestra en pantalla la imagen

--- TP4/tp4.py
+++ TP4/tp4.py
@@ -8,14 +8,15 @@ mode     = True
 ######################## FUNCIONES #######################
 
 def select_image(event, x, y, flags, param):
-    global xi, yi, drawing, mode
+    global xi, yi, drawing, mode#, img
     if(event == cv2.EVENT_LBUTTONDOWN):
         drawing  = True
         (xi, yi) = (x, y)
     elif(event == cv2.EVENT_MOUSEMOVE):
         if(drawing is True):
             img_aux = img.copy()
-            cv2.rectangle(img_aux, (xi,yi), (x,y), (0,255,0), 1)
+            cv2.rectangle(img_aux, (xi,yi), (x,y), (255,0,255), 2)
+            cv2.imshow('image', img_aux )
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
 
@@ -26,9 +27,9 @@ img = cv2.imread ('../TP2/hoja.png', 1)
 #img = np.zeros((512,512,3), np.uint8)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', select_image)
+cv2.imshow('image', img)
 
 while(1):
-    cv2.imshow('image', img )
     k = cv2.waitKey(1) & 0xFF
     if(k == ord('m')):
         mode = not mode
